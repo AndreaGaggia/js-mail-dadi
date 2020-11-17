d5f64498d0501f274d6d6a496375f8600d51a38b
AndreaG <gaggiandrea@gmail.com>
2020-11-17 18:05:07 +0100

    bonus in mail js-code

--- mail.js
+++ mail.js
@@ -6,9 +6,21 @@ var userMail = prompt("Scrivi qui il tuo indirizzo email");
 var mailList = ["pippo@disney.com", "pluto@disney.com", "paperino@disney.com", "topolino@disney.com", "qui@disney.com"];
 
 // secondo, verifico con un for loop la presenza della mail all'interno dell'array e stampo un messaggio in alert
-for (var i = 0; i < mailList.length; i++) {
+// for (var i = 0; i < mailList.length; i++) {
+//   if (userMail == mailList[i]) {
+//     alert("La tua mail risulta registrata!");
+//   }
+// }
+
+// bonus: stampo messaggio alternativo in caso la mail non sia stata registrata
+var registered = false; // variabile "sentinella"
+for (let i = 0; i < mailList.length; i++) {
   if (userMail == mailList[i]) {
-    alert("La tua mail risulta registrata!");
+    registered = true;
   }
 }
-
+if (registered == false) {
+  alert("La mail non risulta registrata :(");
+} else if (registered == true) {
+  alert("La mail risulta registrata!");
+}
